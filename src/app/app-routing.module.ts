6d2cab62d17432b4bae8296bf87e3094eca478ba
grossocinthia <94639520+grossocinthia@users.noreply.github.com>
2022-05-19 18:07:39 -0300

    editar
    implementacion con fallas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { CrearEdComponent } from './componentes/crear-ed/crear-ed.component';
 import { CrearExComponent } from './componentes/crear-ex/crear-ex.component';
 import { CrearProyComponent } from './componentes/crear-proy/crear-proy.component';
+import { EditarProyectoComponent } from './componentes/editar-proyecto/editar-proyecto.component';
 
 
 
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'crearEd', component: CrearEdComponent},
   { path: 'crearEx', component: CrearExComponent},
   { path: 'crearProy', component: CrearProyComponent},
+  { path: 'editarProy', component: EditarProyectoComponent},
   
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 
--- src/app/componentes/editar-proyecto/editar-proyecto.component.ts
+++ src/app/componentes/editar-proyecto/editar-proyecto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Proyecto } from 'src/app/models/proyecto';
+import { ProyectoService } from 'src/app/service/proyectoService';
 
 @Component({
   selector: 'app-editar-proyecto',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-proyecto.component.css']
 })
 export class EditarProyectoComponent implements OnInit {
+ 
 
-  constructor() { }
+  proyectoList: any ;
 
-  ngOnInit(): void {
+  
+
+  constructor(private datosproyecto: ProyectoService, private activatedRoute: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void { 
+   
+      this.datosproyecto.verProyecto().subscribe(data =>{
+      console.log(data);
+      this.proyectoList=data[0];
+      
+    });
+      }
+    
+      onUpdate(id: number) {
+        this.datosproyecto.editarProyecto(id, this.proyectoList).subscribe(
+          data => {
+            console.log(data);
+            this.datosproyecto.editarProyecto;
+            this.ngOnInit();
+        this.router.navigate(['/portfolio']);
+      },
+     
+    );
   }
 
 }
